9d2de3ac75d667106637c2cc74c5ef15135acd01
yuki-js <ayukiyhs@gmail.com>
2017-01-31 21:59:02 +0900

    loadable

--- bundle.js
+++ bundle.js
@@ -17,22 +17,37 @@ $(".skipBtn").on("click",function(){
   var $this=$(this);
   video.currentTime+=($this.attr("data-ms")|0)/1000;
 });
-
-$video.on("timeupdate",function(){
-  seekBar.css("width",(video.currentTime/video.duration*100)+"%");
+var $currTime=$("#currentTime").on('change',function(){
+  video.currentTime=$currTime.val()/1000;
 });
-$("#bar").on("click",function(e){
-  video.currentTime=(e.offsetX/seekBar.width()*video.duration)|0;
+var $speed=$("#speed").on("change",function(){
+  video.playbackRate=$speed.val();
+});
+var $duration=$("#duration").text((video.duration*1000)|0);
+$video.on("timeupdate",function(){
+  $currTime.val((video.currentTime*1000)|0);
+  seekBar.css("width",(video.currentTime/video.duration*100)+"%");
+}).on("durationchange",function(){
+  $duration.text((video.duration*1000)|0);
+}).on("ratechange",function(){
+  $speed.val(video.playbackRate)
+});
+var bar=$("#bar").on("click",function(e){
+  video.currentTime=(e.offsetX/bar.width()*video.duration)|0;
 });
 
 var markerArea=$("#markerArea");
 var markerName=$("#markerName");
+var markerList=$("#markerList");
 $("#addMarker").on("click",function(){
   var time=(video.currentTime*1000)|0;
-  $('<div class="markerContainer"><div class="marker"><div class="time">'+time+'ms</div><div>'+markerName.val()+'</div><a class="delete" href="#"></a></div></div>').appendTo(markerArea).css("left",(video.currentTime/video.duration*100)+"%");
+  $('<div class="markerContainer" data-ms="'+time+'"><div class="marker"><div class="time">'+time+'ms</div></div></div>').appendTo(markerArea).css("left",(video.currentTime/video.duration*100)+"%");
+  $('<div class="markerItem" data-ms="'+time+'"><span class="time">'+time+'ms</span><span class="text">'+markerName.val()+'</span><a class="delete" href="#"></a></div>').appendTo("#markerList");
 });
-markerArea.on("click",".delete",function(){
-  $(this).parent().remove();
+markerList.on("click",".delete",function(){
+  var $this=$(this).parent();
+  $(".markerContainer[data-ms="+$this.attr("data-ms")+"]").remove();
+  $this.remove();
 });
 $("#fast").on("click",function(){
   video.playbackRate*=2;
@@ -40,7 +55,43 @@ $("#fast").on("click",function(){
 $("#slow").on("click",function(){
   video.playbackRate*=0.5;
 });
+var loader=$("#loader");
+var viewer=$("#viewer");
+if(!window.File){
+  $("#dropArea #error").text("File API is not supported!");
+}
+$("#dropArea").on('drop',function(e){
+  e.preventDefault();
+  var files = e.originalEvent.dataTransfer.files
+  video.src=URL.createObjectURL(files[0]);
+  loader.hide();
+  viewer.show();
+  return false;
+}).on('dragover', function() {
+	return false;
+});
+$("#videoFile").on("change",function(){
+  video.src=URL.createObjectURL(this.files[0]);
+  loader.hide();
+  viewer.show();
+});
+var markers=$("#markerArea,#markerAddForm,#markerList");
+$("#hideMarker").on("click",function(){
+  markers.toggle();
+});
+var ctrlArea=$("#controlArea");
+$("#videoArea").on("click",function(){
+  ctrlArea.toggle();
+});
+/*
+to do
 
+marker List table
+show currentTime
+load menu
+zoom
+smartphone
+*/
 
 },{"jquery":2}],2:[function(require,module,exports){
 /*!
